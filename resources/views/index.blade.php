35c1caec72a5bc879bc6464ff84953d87601394e
Arsipa <senya.suvorov.2003@mail.ru>
2022-06-22 16:45:00 +0300

    add header, footer, jb

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,10 @@
 @section('content')
     <div class="container">
         <div class="inner">
-            
+            <section class="jb">
+                <img src="img/jb.png" alt="">
+                <h1>Сервис С PRO - инженерия для вашего дома</h1>
+            </section>
         </div>
     </div>
 @endsection
--- resources/views/layouts/structure.blade.php
+++ resources/views/layouts/structure.blade.php
@@ -17,9 +17,10 @@
         <header class="header">
             <div class="container">
                 <div class="header__wrapper">
-                    <img src="img/logo.svg" alt="Сервис C PRO">
+                    <a href=""><img src="img/logo.svg" alt="Сервис C PRO"></a>
                     <div class="header__links">
-                        
+                        <a href="tel:+79467384837">+7 946 738 48 37</a>
+                        <a href="">Наш телеграм <img src="img/telegram.svg" alt="Телеграм"></a>
                     </div>
                 </div>
             </div>
@@ -30,7 +31,48 @@
         <footer class="footer">
             <div class="container">
                 <div class="footer__wrapper">
-
+                    <div class="footer__top">
+                        <form action="" class="footer__form form">
+                            <h4 class="form__title">Закажите консультацию, оставив нам свою почту. Мы вам всё расскажем
+                            </h4>
+                            <input type="email" placeholder="E-mail*" required>
+                            <input type="text" placeholder="Напишите, что вас интересует">
+                            <button type="submit">Отправить</button>
+                        </form>
+                        <img src="img/footer.png" alt="Оставьте заявку">
+                    </div>
+                    <div class="footer__bottom">
+                        <div class="footer__list-wrapper">
+                            <ul class="footer__list">
+                                <li><a href="">Отопление</a></li>
+                                <li><a href="">Водоснабжение</a></li>
+                                <li><a href="">Автономные канализации</a></li>
+                                <li><a href="">Теплые полы</a></li>
+                            </ul>
+                            <ul class="footer__list">
+                                <li><a href="">Сборка Котельной</a></li>
+                                <li><a href="">Электричесвто</a></li>
+                                <li><a href="">Дизайн проект</a></li>
+                            </ul>
+                        </div>
+                        <div class="footer__bottom-right">
+                            <div class="footer__list-wrapper">
+                                <ul class="footer__list">
+                                    <li><a href="tel:+79467384837">+7 946 738 48 37 <img src="img/phone.svg"
+                                                alt=""></a></li>
+                                    <li><a href="tel:+79467384837">+7 946 738 48 37 <img src="img/phone.svg"
+                                                alt=""></a></li>
+                                </ul>
+                                <ul class="footer__list">
+                                    <li><a href="mailto:mail@mail.com">mail@mail.com <img src="img/mail.svg"
+                                                alt=""></a></li>
+                                    <li><a href="mailto:mail@mail.com">mail@mail.com <img src="img/mail.svg"
+                                                alt=""></a></li>
+                                </ul>
+                            </div>
+                            <a href="">Наш телеграм <img src="img/telegram.svg" alt="Телеграм"></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </footer>
