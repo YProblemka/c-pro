eb91e05340a8c16300ebcdf68d406162dac71d44
Roman Marinichev <rmarinicev@gmail.com>
2022-06-26 12:09:14 +0300

    add seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             OurWorkSeeder::class,
             SettingSeeder::class,
+            AdminSeeder::class,
         ]);
     }
 }
