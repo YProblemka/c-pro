d9992776ca1613d8aa8401a64cf7562164c569a1
Roman Marinichev <rmarinicev@gmail.com>
2022-06-26 12:10:30 +0300

    add AdminAuthController

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -18,7 +18,6 @@ class AdminAuthController extends Controller
     public function login(Request $request)
     {
         $data = $request->only(["login", "password"]);
-        $data["is_admin"] = true;
         if (!auth()->guard("admin")->attempt($data, true)) {
             return redirect(route("admin.login"))->withErrors([
                 "login" => "Неправильные данные"
