bdf2f678b8b9601fd95ed7a4ccdea490291fc9a3
Roman Marinichev <rmarinicev@gmail.com>
2022-06-26 11:57:25 +0300

    auth

---
+++ app/Http/Controllers/AdminAuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $data = $request->only(["login", "password"]);
+        $data["is_admin"] = true;
+        if (!auth()->guard("admin")->attempt($data, true)) {
+            return redirect(route("admin.login"))->withErrors([
+                "login" => "Неправильные данные"
+            ]);
+        }
+        return redirect(route("admin.settings"));
+    }
+
+    public function logout()
+    {
+        Auth::guard("admin")->logout();
+
+        return redirect(route("index"));
+    }
+}
